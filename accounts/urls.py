9b1ae4bf1eacea87f97e97eb54d52b8808b300c3
ruhollah82 <0robena0@gmail.com>
2025-12-27 22:27:30 +0330

    feat: add URL pattern for user invite code generation endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('refresh/', views.CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('invite/create/', views.create_invite_code, name='create_invite'),
+    path('invite/generate/', views.user_create_invite_code, name='user_create_invite'),
 ]
