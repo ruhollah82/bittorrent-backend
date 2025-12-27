63c89c040a0c6eabb850ee602fa886efb51bd4ba
ruhollah82 <0robena0@gmail.com>
2025-12-27 22:28:52 +0330

    feat: add regex validation for info_hash URLs and categories list endpoint

--- torrents/urls.py
+++ torrents/urls.py
@@ -1,18 +1,19 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'torrents'
 
 urlpatterns = [
     path('', views.TorrentListView.as_view(), name='list'),
-    path('<str:info_hash>/', views.TorrentDetailView.as_view(), name='detail'),
-    path('<str:info_hash>/stats/', views.TorrentStatsView.as_view(), name='stats'),
-    path('<str:info_hash>/peers/', views.torrent_peers, name='peers'),
-    path('<str:info_hash>/health/', views.torrent_health, name='health'),
-    path('<str:info_hash>/delete/', views.delete_torrent, name='delete'),
-    path('<str:info_hash>/download/', views.download_torrent, name='download'),
+    re_path(r'^(?P<info_hash>[a-fA-F0-9]{40})/$', views.TorrentDetailView.as_view(), name='detail'),
+    re_path(r'^(?P<info_hash>[a-fA-F0-9]{40})/stats/$', views.TorrentStatsView.as_view(), name='stats'),
+    re_path(r'^(?P<info_hash>[a-fA-F0-9]{40})/peers/$', views.torrent_peers, name='peers'),
+    re_path(r'^(?P<info_hash>[a-fA-F0-9]{40})/health/$', views.torrent_health, name='health'),
+    re_path(r'^(?P<info_hash>[a-fA-F0-9]{40})/delete/$', views.delete_torrent, name='delete'),
+    re_path(r'^(?P<info_hash>[a-fA-F0-9]{40})/download/$', views.download_torrent, name='download'),
     path('upload/', views.upload_torrent, name='upload'),
     path('categories/', views.torrent_categories, name='categories'),
+    path('categories/list/', views.categories_list, name='categories_list'),
     path('popular/', views.torrent_popular, name='popular'),
     path('my-torrents/', views.user_torrents, name='user_torrents'),
 ]
